16d97638fe04af86923c7361d9832bb95acfc5d8
satorinic <niccarlsson@gmail.com>
2017-06-13 11:29:57 +0200

    adding test-> withdraw amount from atm

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,6 +1,10 @@
 class Atm
   attr_accessor :funds
   def initialize
-    @funds = 1000 
+    @funds = 1000
+  end
+
+  def withdraw(amount)
+    @funds -= amount
   end
 end
--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -1,6 +1,11 @@
 require './lib/atm.rb'
 describe Atm do
-  it 'has 1000$ in initialize' do
+  xit 'has $1000 in initialize' do
     expect(subject.funds).to eq 1000
+  end
+
+  it 'funds are reduced at withdraw' do
+    subject.withdraw 50
+    expect(subject.funds).to eq 950
   end
 end
