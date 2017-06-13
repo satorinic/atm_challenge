85127e93b5653f47ec5ed1c53b9ea35e367a717c
satorinic <niccarlsson@gmail.com>
2017-06-13 16:34:04 +0200

    adding refactoring-> private withdraw

--- lib/atm.rb
+++ lib/atm.rb
@@ -1,18 +1,28 @@
 class Atm
   attr_accessor :funds
 
-  def initialize
-    @funds = 1000
-  end
+    def initialize
+      @funds = 1000
+    end
 
-  def withdraw(amount, account)
-    case
-    when amount > account.balance
-      return
-    else
-      @funds -= amount
-      account.balance = account.balance - amount
-      { status: true, message: 'success', date: Date.today, amount: amount }
+    def withdraw(amount, account)
+      case
+      when insufficient_funds_in_account?(amount, account)
+        return
+      else
+        perform_transaction(amount, account)
     end
   end
-end
+
+    private
+
+    def insufficient_funds_in_account?(amount, account)
+      amount > account.balance
+    end
+
+    def perform_transaction(amount, account)
+          @funds -= amount
+          account.balance = account.balance - amount
+          { status: true, message: 'success', date: Date.today, amount: amount }
+    end
+  end
