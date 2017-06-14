1ef4c597087f45dea8791738eab602bfae73b77c
satorinic <niccarlsson@gmail.com>
2017-06-14 17:23:30 +0200

    adding exp card test and implementation

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -2,13 +2,19 @@ require './lib/atm.rb'
 require 'date'
 
 describe Atm do
-  let(:account) { instance_double('Account', pin_code: '1234') }
+  let(:account) { instance_double('Account', pin_code: '1234', exp_date: '04/18') }
 
   before do
 
     allow(account).to receive(:balance).and_return(100)
     allow(account).to receive(:balance=)
    end
+
+    it 'reject withdraw if card is expired' do
+      allow(account).to receive(:exp_date).and_return('12/15')
+      expected_output = { status: false, message: 'card expired', date: Date.today }
+      expect(subject.withdraw(10, '1234', account)).to eq expected_output
+  end
 
     it 'allow withdraw if account has enough balance.' do
        expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
@@ -33,5 +39,10 @@ describe Atm do
       subject.funds = 50
       expected_output = { status: false, message: 'insufficient funds in ATM', date: Date.today }
       expect(subject.withdraw(100, '1234', account)).to eq expected_output
+  end
+
+    it 'reject withdraw if pin is wrong' do
+      expected_output = { status: false, message: 'wrong pin', date: Date.today }
+      expect(subject.withdraw(50, '9999', account)).to eq expected_output
   end
 end
