d37077f1cfac2d2e8d7c0d318d3934acc35ab04c
satorinic <niccarlsson@gmail.com>
2017-06-15 17:19:15 +0200

    adding text and implementation of expire date on card

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,22 @@
 class Account
-  attr_accessor :pin_code, :balance
+  STANDARD_VALIDITY_YRS = 5
+
+  attr_accessor :pin_code, :balance, :exp_date
 
   def initialize
     @pin_code = generate_pin
     @balance = 0
+    @exp_date = set_exp_date
+
 
   end
 
   private
   def generate_pin
     rand(1000..9999)
+  end
+
+  def set_exp_date
+    Date.today.next_year(Account::STANDARD_VALIDITY_YRS).strftime('%m/%y')
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,9 +3,18 @@ require 'date'
 
 describe Account do
 
-  it 'check for the length of the pin code' do
+  it 'check the length of the pin code' do
    number = 1234
    number_length = Math.log10(number).to_i + 1
    expect(number_length).to eq 4
   end
- end
+
+  it 'check if the account balance is 0 on initialize' do
+  expect(subject.balance).to eq 0
+  end
+
+  it 'check if the card has an expiry date on initialize' do
+    expected_date = Date.today.next_year(5).strftime("%m/%y")
+    expect(subject.exp_date).to eq expected_date
+  end
+end
