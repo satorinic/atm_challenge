4f951f67b1b08b0105dd9d0e99fbbef2bf3bcf48
satorinic <niccarlsson@gmail.com>
2017-06-15 10:14:43 +0200

    adding bills test

--- spec/atm_spec.rb
+++ spec/atm_spec.rb
@@ -24,7 +24,12 @@ describe Atm do
     end
 
     it 'allow withdraw if account has enough balance.' do
-       expected_output = { status: true, message: 'success', date: Date.today, amount: 45 }
+       expected_output = {
+         status: true,
+         message: 'success',
+         date: Date.today,
+         amount: 45,
+         bills: [20, 10, 5] }
        expect(subject.withdraw(45, '1234', account)).to eq  expected_output
     end
 
