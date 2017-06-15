3370a5a166821248aced7440635e7667051afefb
satorinic <niccarlsson@gmail.com>
2017-06-15 19:52:01 +0200

    adding test and implementation for deactivating account

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,17 @@
 class Account
   STANDARD_VALIDITY_YRS = 5
 
-  attr_accessor :pin_code, :balance, :exp_date
+  attr_accessor :pin_code, :balance, :exp_date, :account_status
 
   def initialize
     @pin_code = generate_pin
     @balance = 0
     @exp_date = set_exp_date
+    @account_status = :active
+  end
 
-
+  def deactivate
+    @account_status = :deactivated
   end
 
   private
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,17 +4,27 @@ require 'date'
 describe Account do
 
   it 'check the length of the pin code' do
-   number = 1234
-   number_length = Math.log10(number).to_i + 1
-   expect(number_length).to eq 4
+    number = 1234
+    number_length = Math.log10(number).to_i + 1
+    expect(number_length).to eq 4
   end
 
   it 'check if the account balance is 0 on initialize' do
-  expect(subject.balance).to eq 0
+    expect(subject.balance).to eq 0
   end
 
   it 'check if the card has an expiry date on initialize' do
     expected_date = Date.today.next_year(5).strftime("%m/%y")
     expect(subject.exp_date).to eq expected_date
   end
+
+  it 'check if the account has active status on initialize' do
+    expect(subject.account_status).to eq :active
+  end
+
+#using instance method because it makes it easier
+  it 'check if account deactivates (using Instance method)' do
+    subject.deactivate
+    expect(subject.account_status).to eq :deactivated
+    end
 end
